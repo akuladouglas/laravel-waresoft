935269344a8b803e5a63400db85a39d61e28a092
douglas <akulad19@gmail.com>
2018-08-08 22:06:04 +0300

    added new vs returning customers

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Carbon\Carbon;
 
 class CustomerController extends BaseController
 {
