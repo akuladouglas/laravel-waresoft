2994a04292ab6e59c4f87eabb8b2bc7416f7ba91
douglas <akulad19@gmail.com>
2018-11-16 09:10:05 +0300

    updated api controller

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -30,17 +30,22 @@ class ApiController extends BaseController
     $date = Carbon::parse($_POST["date"])->format("Y-m-d h:m:s");
     $from = $_POST["from"];
     
-    if (strpos($text, 'claim') !== false ||strpos($text, 'claiming') !== false) {
-       $smsRedemption = new SmsRedemptions();
-       $smsRedemption->redeemPoints($from, $text);
-    }
-    
-    if (strpos($text, 'offer') !== false /*|| strpos($text, 'offers') !== false || strpos($text, 'offering') !== false*/) {
+    if (strpos($text, 'offers') !== false /*|| strpos($text, 'offers') !== false || strpos($text, 'offering') !== false*/) {
        $smsText = "Hi Beauty, we have received your message and we will call you shortly.";
        $sms = new SmsService();
        $sms->sendNewSms("254". substr($from, -9), $smsText);
     }
     
+    if (strpos($text, 'claim') !== false) {
+       $smsRedemption = new SmsRedemptions();
+       $smsRedemption->redeemPoints($from, $text);
+    }
+    
+    if(strpos($text, 'claiming') !== false){
+      $smsRedemption = new SmsRedemptions();
+      $smsRedemption->redeemPoints($from, $text);
+    }
+
     $smsLead = new SmsLead();
     $smsLead->sms_to = $to;
     $smsLead->sms_from = $from;
