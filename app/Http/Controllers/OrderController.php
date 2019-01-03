d28417a6aa46c8a300cb9594f3c47ae77028b9fc
douglas <akulad19@gmail.com>
2019-01-03 11:49:39 +0300

    updated cyfe dash controller

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -266,7 +266,7 @@ class OrderController extends BaseController
             $originator_date = "2018-12-31";
         }
         
-        $originator_date = "2018-10-31";
+        $originator_date = "2018-11-31";
         
         $formatted_date = Carbon::parse($originator_date)->format('Y-m-d\TH:i:s');
 
